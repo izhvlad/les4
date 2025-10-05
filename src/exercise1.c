f87389c3f7204dcfeb722b98dad325536d84ca25
Ижболдин Владислав Александрович ИП-513 <izh_vlad@inbox.ru>
2025-10-05 21:38:33 +0700

    пофиксил первое

--- src/exercise1.c
+++ src/exercise1.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 
 int main(int argc, char** argv){
-  float res = 0;
+  double res = 0;
    double che[10];
     for(int i = 0; i < 10; ++i){
         scanf("%lf", &che[i]);
     }
     for (int i = 0; i < 10; i++){
-res += che[i];
+    res += che[i];
     }
-    printf("%.2lf", res/10);
+    res *= 10;
+
+
+    printf("%.2lf", res/100);
 return 0;
 
 }
