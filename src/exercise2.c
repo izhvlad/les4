f209b9a5f5f5fdff9afcc0dcd417906647757a7d
Ижболдин Владислав Александрович ИП-513 <izh_vlad@inbox.ru>
2025-10-06 17:56:12 +0700

    пофиксил везде вывод

--- src/exercise2.c
+++ src/exercise2.c
@@ -7,7 +7,7 @@ int main(int argc, char** argv){
         scanf("%lf", &che[i]);
     }
         for (int i = 0; i < 10; i++){
-    printf("%.0f ", che[9 - i]);
+    printf("%.3f ", che[9 - i]);
 
         }
 
--- src/exercise3.c
+++ src/exercise3.c
@@ -12,11 +12,11 @@ for(int i = 0; i < 10; i++ ){
 if (k >= 0){
  for(int i = 0; i < 10; i++){
   if (i < k){
-    printf("%.0f ", ara[10 + i - k] );
+    printf("%.3f ", ara[10 + i - k] );
 }
   else if(k <= i)
  {
-     printf("%.0f ", ara[i - k] );
+     printf("%.3f ", ara[i - k] );
  }
 }
 }
@@ -24,12 +24,12 @@ if(k < 0)
 {
  for(int i = 0; i < 10 ; i++){
   if (i - k < 10 ){
-    printf("%.0f ", ara[ -k + flug]);
+    printf("%.3f ", ara[ -k + flug]);
       flug++;                             // 0 1 2 3 4 5 6 7 8 9
                                          //  1 2 3 4 5 6 7 8 9 10
 }
 else if (i - k >= 10){
-printf("%.0f ", ara[ i - flug]);
+printf("%.3f ", ara[ i - flug]);
 
 }
  }
