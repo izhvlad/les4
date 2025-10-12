12d6e8d044abc774ba3d6b30ffffe532e2828a6b
Ижболдин Владислав Александрович ИП-513 <izh_vlad@inbox.ru>
2025-10-12 15:32:22 +0700

    Сделал домашнее №5

--- src/array_function.c
+++ src/array_function.c
@@ -1,8 +1,40 @@
 #include "../include/array_function.h"
 
-void join_int_arrays(int *src1, size_t size_src1, int *src2, size_t size_src2, int *dest) {}
+void join_int_arrays(int *src1, size_t size_src1, int *src2, size_t size_src2, int *dest) {
+    int size_dest = size_src1 + size_src2;
+  for(int i = 0; i < size_dest; i++ ){
+    dest[i] = src1[i];
+        if(size_src1 < i + 1 ){
+            dest[i] = src2[i - size_src1];
+        }
+    }
+}
 
-void join_and_sort_int_arrays(int* src1, size_t size_src1, int* src2, size_t size_src2, int* dest) {}
+void join_and_sort_int_arrays(int* src1, size_t size_src1, int* src2, size_t size_src2, int* dest) {
+    int size_dest = size_src1 + size_src2;
+   join_int_arrays(src1, size_src1, src2, size_src2, dest);
+    for(int i = 0; i < size_dest - 1; i++) {
+        for(int b = 0; b < size_dest - i - 1; b++) {
+            if(dest[b] > dest[b + 1]) {
+                int temp_dest = dest[b];
+                dest[b] = dest[b + 1];
+                dest[b + 1] = temp_dest;
+            }
+        }
+    }
+}
 
-void get_min_and_max_from_int_array(int* src, size_t size, int* min, int* max) {}
+void get_min_and_max_from_int_array(int* src, size_t size, int* min, int* max) {
+    for(int i = 0; i < size - 1; i++) {
+        for(int b = 0; b < size - i - 1; b++) {
+            if(src[b] > src[b + 1]) {
+                int temp_dest = src[b];
+                src[b] = src[b + 1];
+                src[b + 1] = temp_dest;
+            }
+        }
+    }
+    *min = src[0];
+    *max = src[size - 1];
+}
 
